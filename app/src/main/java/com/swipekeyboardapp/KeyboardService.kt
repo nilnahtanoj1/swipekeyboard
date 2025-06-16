aae8c99dbb6c47c2f1df74b8bb8f864e1bd90d49
Jonathan <nilnahtanoj1@gmail.com>
2025-06-15 21:44:55 -0700

    Added keyboardservice, keyboardview, method, querty and androidmanifest

--- app/src/main/java/com/swipekeyboardapp/KeyboardService.kt
+++ app/src/main/java/com/swipekeyboardapp/KeyboardService.kt
@@ -1,3 +1,45 @@
 //extends InputMethodService
 //core engine of keyboard
 //handles lifecycle events, connects to input field, controls interactions
+package com.mykeyboardapp
+
+import android.inputmethodservice.InputMethodService
+import android.inputmethodservice.Keyboard
+import android.inputmethodservice.KeyboardView
+import android.view.View
+
+class KeyboardService : InputMethodService(), KeyboardView.OnKeyboardActionListener {
+
+    private lateinit var kv: KeyboardView
+    private lateinit var keyboard: Keyboard
+
+    override fun onCreateInputView(): View {
+        kv = layoutInflater.inflate(R.layout.keyboard_view, null) as KeyboardView
+        keyboard = Keyboard(this, R.xml.qwerty)
+        kv.keyboard = keyboard
+        kv.setOnKeyboardActionListener(this)
+        return kv
+    }
+
+    override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
+        val ic = currentInputConnection
+        when (primaryCode) {
+            Keyboard.KEYCODE_DELETE -> ic.deleteSurroundingText(1, 0)
+            Keyboard.KEYCODE_DONE -> ic.sendKeyEvent(
+                android.view.KeyEvent(android.view.KeyEvent.ACTION_DOWN, android.view.KeyEvent.KEYCODE_ENTER)
+            )
+            else -> {
+                val code = primaryCode.toChar()
+                ic.commitText(code.toString(), 1)
+            }
+        }
+    }
+
+    override fun onPress(primaryCode: Int) {}
+    override fun onRelease(primaryCode: Int) {}
+    override fun onText(text: CharSequence?) {}
+    override fun swipeLeft() {}
+    override fun swipeRight() {}
+    override fun swipeDown() {}
+    override fun swipeUp() {}
+}
--- app/src/main/java/com/swipekeyboardapp/KeyboardView.kt
+++ app/src/main/java/com/swipekeyboardapp/KeyboardView.kt
@@ -1,2 +1,15 @@
 //extends KeyboardView
 //manages rendering keys and capturing touch events
+<?xml version="1.0" encoding="utf-8"?>
+<android.inputmethodservice.KeyboardView xmlns:android="http://schemas.android.com/apk/res/android"
+android:id="@+id/keyboard"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:layout_alignParentBottom="true"
+android:keyBackground="@drawable/key_background"
+android:keyPreviewLayout="@layout/key_preview"
+android:keyTextColor="@color/black"
+android:keyTextSize="18sp"
+android:labelTextSize="14sp"
+android:shadowColor="@color/gray"
+android:shadowRadius="0.5" />
